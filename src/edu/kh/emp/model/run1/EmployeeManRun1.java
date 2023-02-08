bab2acaaed61174db5f21d8be1d996b42d1edac3
LEE JIN YOUNG <jinijnleelee@gmail.com>
2023-02-09 02:45:02 +0900

    JDBC연습

---
+++ src/edu/kh/emp/model/run1/EmployeeManRun1.java
@@ -0,0 +1,14 @@
+package edu.kh.emp.model.run1;
+
+import edu.kh.emp.view1.EmployeeView1;
+
+public class EmployeeManRun1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		new EmployeeView1().displayMenu();
+		
+		
+
+	}
+}
